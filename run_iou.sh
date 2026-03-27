f96f4494880b7ff01c42b38e63716061c2e4215b
shanicky4ever <shanicky4ever@gmail.com>
2026-03-27 23:58:18 +0800

    missing shells

---
+++ run_iou.sh
@@ -0,0 +1,14 @@
+export PYTHONPATH=$(pwd)
+export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6
+start_timestep=0 # start frame index for training
+end_timestep=-1 # end frame index, -1 for the last frame
+
+python tools/train.py \
+    --config_file configs/omnire_iou.yaml \
+    --output_root output \
+    --project recon \
+    --run_name recon_$1_iou \
+    dataset=waymo/3cams \
+    data.scene_idx=$1 \
+    data.start_timestep=0 \
+    data.end_timestep=-1
